03cfbdac050b3c122dc4e1a98c53db87fe6b9cbd
Praval Singh <pravalsingh305@gmail.com>
2026-02-16 20:00:20 +0530

    fix redis syntax & secure env

--- src/redis/redis-client.js
+++ src/redis/redis-client.js
@@ -1,9 +1,8 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379
-});
+const redis = new Redis(
+  process.env.REDIS_URL
+);
 
 redis.on("connect", () => {
   console.log("⚡ Redis Connected");
